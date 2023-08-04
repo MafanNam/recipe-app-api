463bca4097badc98264ac134608bb1639f6fe2cc
Maxim Manzik <kepkpkep@gmail.com>
2023-08-04 17:25:16 +0300

    compleat user token authentication, test, update user

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # ADDONS
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
 
+    # MY SERVICES
     'core',
     'user'
 ]
--- app/user/urls.py
+++ app/user/urls.py
@@ -8,5 +8,8 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view(), name='create')
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
+
+    path('token/', views.CreateTokenView.as_view(), name='token'),
 ]
