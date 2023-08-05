3bbd5394314a86a9872d31ef9a78dbf44d04f5dc
Maxim Manzik <kepkpkep@gmail.com>
2023-08-05 21:22:22 +0300

    create tag model, test, mixins viewSet(list, update)

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -8,10 +8,11 @@ from django.urls import (
 
 from rest_framework.routers import DefaultRouter
 
-from recipe import views
+from . import views
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
 
 app_name = 'recipe'
 
